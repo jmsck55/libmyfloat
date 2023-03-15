29fc4e5e263405369d878443cabf9835b142dc6e
James Cook <jmsck55@gmail.com>
2023-03-15 16:48:25 -0400

    update comment

--- source/buildw.bat
+++ source/buildw.bat
@@ -1,5 +1,5 @@
 rem Copyright (c) 2023 James Cook
- 
+rem uses cl.exe from https://visualstudio.microsoft.com
 cl /LD libmyfloat.c /DMY_EXPORT_DLL
 cl /Femain_test.exe main.c /DMY_IMPORT_DLL
 rem main_test.exe
