8676284e38503cc91b23151d0fbeaaeaffc44900
jmsck55 <jmsck55@gmail.com>
2023-03-16 07:59:29 -0400

    updated windows bins

--- source/buildw.bat
+++ source/buildw.bat
@@ -1,7 +1,5 @@
 rem Copyright (c) 2023 James Cook
 rem uses cl.exe from https://visualstudio.microsoft.com
 cl /LD libmyfloat.c /DMY_EXPORT_DLL
-rem cmake
-rem See also: https://learn.microsoft.com/en-us/cpp/build/walkthrough-creating-and-using-a-dynamic-link-library-cpp?view=msvc-170
-cl /Femain_test.exe main.c /DMY_IMPORT_DLL
+cl /Femain_test.exe main.c /DMY_IMPORT_DLL /link libmyfloat.lib
 rem main_test.exe
