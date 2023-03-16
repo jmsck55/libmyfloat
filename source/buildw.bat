8450b37a8037f26a7256c4837201a966693574f2
James Cook <jmsck55@gmail.com>
2023-03-16 06:28:05 -0400

    update

--- source/buildw.bat
+++ source/buildw.bat
@@ -1,5 +1,7 @@
 rem Copyright (c) 2023 James Cook
 rem uses cl.exe from https://visualstudio.microsoft.com
 cl /LD libmyfloat.c /DMY_EXPORT_DLL
+rem cmake
+rem See also: https://learn.microsoft.com/en-us/cpp/build/walkthrough-creating-and-using-a-dynamic-link-library-cpp?view=msvc-170
 cl /Femain_test.exe main.c /DMY_IMPORT_DLL
 rem main_test.exe
