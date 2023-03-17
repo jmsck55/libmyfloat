cfa867fcd80382b8418f35a332ce279fc3ba0271
jmsck55 <jmsck55@gmail.com>
2023-03-17 13:34:28 -0400

    update

--- source/buildw.bat
+++ source/buildw.bat
@@ -1,5 +1,11 @@
 rem Copyright (c) 2023 James Cook
 rem uses cl.exe from https://visualstudio.microsoft.com
+rem Compile without dll:
+cl libmyfloat.c main.c
+main.exe
+pause
+rem Compile with dll:
 cl /LD libmyfloat.c /DMY_EXPORT_DLL
 cl /Femain_test.exe main.c /DMY_IMPORT_DLL /link libmyfloat.lib
-rem main_test.exe
+main_test.exe
+pause
